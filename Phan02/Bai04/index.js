58422b4cc7ba310bb0f06ead92afe75a3fa12fd2
hungdinh1212 <dinhhung12122003@gmai.com>
2025-04-06 11:37:46 +0700

    Hoan thanh bai 4 Phan 2

--- Phan02/Bai04/index.js
+++ Phan02/Bai04/index.js
@@ -1,1 +1,11 @@
 const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Xin chào từ Node.js + Docker Compose!');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
